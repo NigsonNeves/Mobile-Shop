cf708499c29f99389d04c28abeece488a4463b42
Theo Poulet Souchard <theopouletsouchard@gmail.com>
2018-09-15 00:11:41 +0200

    Update swagger & api cross-origin

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ firebase.initializeApp(firebase_config)
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
