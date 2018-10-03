b4495eca7b658f3aeaecbec6b0c1d397327316f2
Theo Poulet Souchard <theopouletsouchard@gmail.com>
2018-10-03 18:02:43 +0200

    Begin route

---
+++ src/routes/orders.js
@@ -0,0 +1,8 @@
+const j_response      = require('../json_response.js')
+const firebase_errors = require('../firebase_error.js')
+
+module.exports = function(app, firebase) {
+  app.post('/shops/:shop_id/orders', function(req, res) {
+    
+  })
+}
