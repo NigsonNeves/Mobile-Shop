9af560099e1e7041f37bcfddfc0e8bc340b69832
Theo Poulet Souchard <theopouletsouchard@gmail.com>
2018-10-03 19:27:02 +0200

    Add route to create new shop

--- src/routes/shops.js
+++ src/routes/shops.js
@@ -1,6 +1,7 @@
 const j_response      = require('../json_response.js')
 const firebase_errors = require('../firebase_error.js')
 const Shop            = require('../models/Shop.ts')
+const ShopType        = require('../enums/ShopType.ts')
 
 module.exports = function(app, firebase_admin) {
   const collection = firebase_admin.firestore().collection(Shop.collection_name)
@@ -22,6 +23,26 @@ module.exports = function(app, firebase_admin) {
     })
     .catch(function(err) {
       res.status(400).send(j_response.format(400, err, null))
+    })
+  })
+
+  app.post('/shops', function(req, res) {
+    const name      = req.body.name
+    const type      = req.body.types
+    const latitude  = req.body.latitude
+    const longitude = req.body.longitude
+    var new_shop    = new Shop(name, [ShopType[type]])
+
+    if (!name || !type || !latitude || !longitude)
+      res.status(400).send(j_response.generic(400))
+
+    new_shop.set_position(latitude, longitude)
+    new_shop.get_collection().doc().set(new_shop.prepare())
+
+    .then(function(doc) {
+      res.status(200).send(j_response.format(200, 'Shop successfully created', new_shop.prepare()))
+    }).catch(function(error) {
+      res.status(400).send(j_response.generic(400))
     })
   })
 }
