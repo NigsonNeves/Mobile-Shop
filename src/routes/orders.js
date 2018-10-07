8a7b364eadbd0d10bc148eade61a977feb6db5d2
Theo Poulet Souchard <theopouletsouchard@gmail.com>
2018-10-07 17:53:33 +0200

    Add user orders get route

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,6 +1,7 @@
 const j_response      = require('../json_response.js')
 const firebase_errors = require('../firebase_error.js')
 const Shop            = require('../models/Shop.ts')
+const User            = require('../models/User.ts')
 const Order           = require('../models/Order.ts')
 const Product         = require('../models/Product.ts')
 
@@ -18,26 +19,55 @@ module.exports = function(app, firebase) {
     var new_order   = new Order(user_id, shop_id)
     var new_product = new Product(null, null)
 
-    new_shop.get_by('id', shop_id).then((doc) => {
-      if (doc == null) {
+    new_shop.get_by('id', shop_id).then((docs) => {
+      if (docs == null) {
         res.status(404).send(j_response.format(404, 'Shop not found', null))
       } else {
         new_product.get_by_ids(JSON.parse(products))
-        .then(function(docs) {
-          new_order.set_products(docs.map(doc => doc.id))
-          new_order.set_price(docs.map(doc => doc.price).reduce(price_reducer))
-          new_order.get_collection().doc().set(new_order.prepare())
-          .then(function(result) {
-            res.status(200).send(j_response.format(200, 'Order successfully created', new_order.prepare()))
-          })
-          .catch(function(err) {
-            res.status(500).send(j_response.format(500, "Error: can't create order", null))
-          })
+        .then(function(docss) {
+          if (docss.length > 0) {
+            new_order.set_products(docss.map(docs => docs.id))
+            new_order.set_price(docss.map(docs => docs.price).reduce(price_reducer))
+            new_order.get_collection().docs().set(new_order.prepare())
+            .then(function(result) {
+              res.status(200).send(j_response.format(200, 'Order successfully created', new_order.prepare()))
+            })
+            .catch(function(err) {
+              res.status(500).send(j_response.format(500, "Error: can't create order", null))
+            })
+          } else {
+            res.status(404).send(j_response.format(404, "Error: some product not found", null))
+          }
         })
         .catch(function(err) {
           res.status(500).send(j_response.generic(500))
         })
       }
+    })
+  })
+
+  app.get('/users/:user_id/orders', function(req, res) {
+    const user_id   = req.params.user_id
+    const new_user  = new User(null)
+    const new_order = new Order(null, null)
+
+    new_user.get_by('id', user_id).then((docs) => {
+      if (docs != null) {
+        new_order.get_by('user', user_id).then((docs) => {
+          console.log(docs)
+          res.status(200).send(j_response.format(200, 'Success', docs))
+        })
+        .catch((err) => {
+          console.log(err)
+          res.status(500).send(j_response.generic(500))
+        })
+      } else {
+        res.status(404).send(j_response.format(404, 'User not found', null))
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).send(j_response.generic(500))
     })
   })
 }
