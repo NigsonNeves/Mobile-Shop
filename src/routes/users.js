c721b42bc1a5153de91535617e9f19a338f8ca20
Nigson <neves_n@etna-alternance.net>
2018-11-01 21:07:40 +0100

    master changes

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,7 @@ const j_response      = require('../json_response.js')
 const firebase_errors = require('../firebase_error.js')
 const admin = require('firebase-admin');
 
-module.exports = function(app, firebase) {
+module.exports = function(app, firebase, firebase_admin) {
   app.get('/users/:user_id', function(req, res) {
     const id_user   = req.params.user_id
     const new_user  = new User(null)
@@ -33,15 +33,14 @@ module.exports = function(app, firebase) {
 
     firebase.auth().createUserWithEmailAndPassword(email, passwd)
     .then(function(userData) {
-      const content   = j_response.format(200, "Successfully created", { email: email })
-      const new_user  = new User(userData.user.uid)
+      var new_user  = new User(userData.user.uid)
 
-      if (first_name) new_user.set_first_name(first_name)
-      if (name) new_user.set_name(name)
-
+      new_user.set_email(email)
+      new_user.set_first_name(first_name)
+      new_user.set_name(name)
       new_user.get_collection().doc().set(new_user.prepare())
 
-      res.status(content.status).send(content)
+      res.status(200).send(j_response.format(200, "Successfully created", { email: email }))
     })
     .catch(function(error) {
       const error_code  = firebase_errors.code(error.code)
@@ -52,11 +51,13 @@ module.exports = function(app, firebase) {
   })
 
   app.patch('/users/:user_id', function(req, res) {
+    const user_id     = req.params.user_id
     const first_name  = req.body.first_name
     const name        = req.body.name
     const picture_url = req.body.picture_url
     const email       = req.body.email
     const password    = req.body.password
+<<<<<<< HEAD
     const id_user     = req.params.user_id
     const authData    = {}
     const new_user    = new User(null)
@@ -109,6 +110,36 @@ module.exports = function(app, firebase) {
           }).catch(function(error) {
             console.log("Error updating user:", error);
           });
+=======
+    var new_user      = new User(null)
+    var auth_data     = {}
+
+    new_user.get_by('id', user_id).then((docs) => {
+      if (!docs) {
+        res.status(404).send(j_response.format(404, `User ${user_id} not found`, null))
+      } else {
+        const user_uid = docs[0].uid;
+        new_user = User.map(docs[0])
+
+        new_user.set_first_name(first_name)
+        new_user.set_name(name)
+        new_user.set_picture_url(picture_url)
+
+        if (email) auth_data.email = email.toString().trim()
+        if (password) auth_data.password = password.toString().trim()
+
+        new_user.set_id(user_id)
+        new_user.set_uid(user_uid)
+
+        firebase_admin.auth().updateUser(user_uid,auth_data).then(function(user) {
+          var datas = { auth: user.toJSON(), user: new_user.prepare() }
+          new_user.get_collection().doc(docs[1]).update(new_user.prepare())
+
+          res.status(200).send(j_response.format(200, 'User successfully updated', datas))
+        }).catch(function(error) {
+          res.status(422).send(j_response.format(422, "Error updating user", null))
+        })
+>>>>>>> 47e3a4ecfce6543fe68dedb6e4225ae025f8cfc7
       }
     }).catch((err) => {
       console.log(err)
